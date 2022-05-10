00f5e74429db84d8e8e1d214d90b44e4fe341625
DiSalvio <stephendisalvio@protonmail.com>
2022-05-10 17:01:21 +0800

    fix import vue test utils

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -1,5 +1,5 @@
 import TestComponent from '@/test.vue'
-import { mount } from @vue/test-utils
+import { mount } from '@vue/test-utils'
 
 test('first spec can import vue components', () => {
   console.log(TestComponent)
