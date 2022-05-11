7d867232c8375859d8a330940219ef28bb94bc12
DiSalvio <stephendisalvio@protonmail.com>
2022-05-11 13:36:58 +0800

    add test component specs for props and html template, set up mount and shallow mount tests for list component

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -1,6 +1,48 @@
 import TestComponent from '@/test.vue'
-import { mount } from '@vue/test-utils'
+import ListComponent from '@/list.vue'
+import { mount, shallowMount } from '@vue/test-utils'
 
-test('first spec can import vue components', () => {
-  console.log(TestComponent)
+describe('mounted test component', () => {
+
+  test('has correct default prop value', () => {
+    const wrapper = mount(TestComponent)
+
+    expect(wrapper.props().value).toContain('Test')
+  })
+
+  test('prop value can be set correctly', () => {
+    const wrapper = mount(TestComponent, {
+      propsData: {
+        value: 'You'
+      }
+    })
+
+    expect(wrapper.props().value).toContain('You')
+  })
+
+  test('html matches snapshot with default prop value', () => {
+    const wrapper = mount(TestComponent)
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('html matches snapshot with default prop value', () => {
+    const wrapper = mount(TestComponent, {
+      propsData: {
+        value: 'You'
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
+
+describe('list component', () => {
+  test('mount', () => {
+    const wrapper = mount(ListComponent)
+  })
+
+  test('shallow mount', () => {
+    const wrapper = shallowMount(ListComponent)
+  })
 })
