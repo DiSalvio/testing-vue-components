1fadc5cf56174f3f0ccd6c2e10e16cd04e6e0208
DiSalvio <stephendisalvio@protonmail.com>
2022-05-17 15:17:30 +0800

    Add tests for alert mount and destroy lifecycle hooks

--- specs/alert.spec.js
+++ specs/alert.spec.js
@@ -1,2 +1,28 @@
 import AlertMessage from '@/alert-message'
 import { mount } from '@vue/test-utils'
+
+describe('lifecycle hooks', () => {
+  it('mounted assigns interval', () => {
+    const { vm } = mount(AlertMessage)
+    expect(vm.interval).not.toBe(undefined)
+  })
+
+  it('counter works', () => {
+    jest.useFakeTimers()
+    const { vm } = mount(AlertMessage)
+    expect(vm.counter).toBe(0)
+    jest.advanceTimersByTime(1000)
+    expect(vm.counter).toBe(1)
+    jest.advanceTimersByTime(4000)
+    expect(vm.counter).toBe(5)
+  })
+
+  it('destroys component after 5 seconds', () => {
+    const beforeDestroySpy = jest.spyOn(AlertMessage, 'beforeDestroy')
+    jest.useFakeTimers()
+    const { vm } = mount(AlertMessage)
+    vm.counter = vm.timer - 1
+    jest.advanceTimersByTime(1000)
+    expect(beforeDestroySpy).toHaveBeenCalled()
+  })
+})
