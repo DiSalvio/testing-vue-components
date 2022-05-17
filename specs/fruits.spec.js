b552b944a832beff610abef643d967d136956a5b
DiSalvio <stephendisalvio@protonmail.com>
2022-05-17 16:36:36 +0800

    add DOM element tests to see fruit basket list updates elements correctly when data is added

--- specs/fruits.spec.js
+++ specs/fruits.spec.js
@@ -1,2 +1,31 @@
-import  FruitBasket from '@/fruit-basket'
+import FruitBasket from '@/fruit-basket'
 import { mount } from '@vue/test-utils'
+
+function sleep(ms) {
+  return new Promise(resolve => { 
+    setTimeout(resolve, ms)
+  })
+}
+
+describe('dom object', () => {
+  it('add fruits to basket', async () => {
+    const wrapper = mount(FruitBasket)
+
+    const input = wrapper.find('input')
+    const button = wrapper.find('button')
+
+    expect(wrapper.findAll('li').length).toBe(0)
+
+    input.element.value = 'banana'
+    input.trigger('input')
+    expect(wrapper.vm.fruit).toBe('banana')
+
+    button.trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.fruit).toBe("")
+    expect(wrapper.vm.basket).toEqual(expect.arrayContaining(['banana']))
+    expect(wrapper.findAll('li').length).toBe(1)
+  })
+
+
+})
