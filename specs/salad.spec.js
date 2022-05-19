840c42f5ccaa44a68d9d1e7c7aec9c6e2a7843d3
DiSalvio <stephendisalvio@protonmail.com>
2022-05-19 11:21:16 +0800

    Test vuex store with vue test utils

--- specs/salad.spec.js
+++ specs/salad.spec.js
@@ -1,6 +1,30 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
 
 import SaladBowlComponent from '@/salad-bowl'
 import saladStore from '@/store/salad-store'
+
+const TestVue = createLocalVue()
+TestVue.use(Vuex)
+const store = new Vuex.Store(saladStore)
+
+test('store is loaded', () => {
+  const store = new Vuex.Store(saladStore)
+  const wrapper = mount(SaladBowlComponent, {
+    localVue: TestVue,
+    store
+  })
+  store.state.salad.push('cucumber')
+  expect(wrapper.vm.salad).toEqual(['cucumber'])
+})
+
+test('store adds ingredient', () => {
+  const store = new Vuex.Store(saladStore)
+  const wrapper = mount(SaladBowlComponent, {
+    localVue: TestVue,
+    store
+  })
+  wrapper.vm.addIngredient('tomato')
+  expect(wrapper.vm.salad).toEqual(['tomato'])
+})
